c05c334e7abf76e4d5a3626742c0a38791e76528
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-08-17 14:45:52 -0600

    added questions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
+//Variables 
+
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
+var initalsEl = document.getElementById()
 
 //putting in initals saving to local storage 
 function renderLastRegistered() {
@@ -44,21 +47,73 @@ function countdown() {
     }
 //selecting an answer pushes it to next question
 
-function nextQuestion() {
-    if (next === true)
+// function nextQuestion() {
+//     if (next === true)
 
+// }
+
+// keeping score if the answer is true add 10 points =+
+var possibilities = "";
+
+if (answer === true) {
+  10;
 }
-
-// keeping score
-
-//timer decreasing with incorrect answer
+//timer decreasing with incorrect answer if answer not coorrect decrease 10 seconds
 
 //timer finishes and game over
 
 //all questions answered game over
 
 //presenting score at the the end 
-
+var message =
+  'Your score is [score]';
+  function displayMessage() {
+    var wordCount = 0;
+  
+    // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds
+    var msgInterval = setInterval(function() {
+      if (words[wordCount] === undefined) {
+        clearInterval(msgInterval);
+      } else {
+        mainEl.textContent = words[wordCount];
+        wordCount++;
+      }
+    }, 300);
+  }
 //local storage saving score and initials
 
-  
+//questions
+
+var questions = [
+{
+    title: "What is Rose Quartz known for?" ,
+    choices: ["Helps with liver", "Unconditional love and healing", "Forgiveness", "Good Energy"],
+    answer: "Unconditional love and healing"
+},
+{
+    title: "What is Jasper known for??" ,
+    choices: ["Negative Energ", "A love connection", "Supreme Nurturer", "Conductivity"],
+    answer: "Supreme Nurturer"
+},
+{
+    title: "What is Obsedian known for?" ,
+    choices: ["Shield against Physical and emotional negativity", "Empowerment", "Balance emotions", "Brings joy"],
+    answer: "Shield against Physical and emotional negativity"
+},
+{
+    title: "What is Led Zeppelin known for?" ,
+    choices: ["being the best rock", "brings a motivational boost", "Stairway to Heaven", "Productivity"],
+    answer: "Stairway to Heaven"
+}, 
+{
+    title: "What is Citrine known for?" ,
+    choices: ["Helps with heart", "keeps away demons", "Forgiveness", "release negative traits from your life"],
+    answer: "release negative traits from your life"
+},
+{
+    title: "What is Turquoise known for?" ,
+    choices: ["heal the mind, body, and soul", "enhance mindful qualities", "purifying", "spiritual wisdom"],
+    answer: "heal the mind, body, and soul"
+}
+];
+
