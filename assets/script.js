1f98d0bec5ab05e1f6abaf51a03d395ae3290646
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-08-17 14:48:53 -0600

    started end game function

--- assets/script.js
+++ assets/script.js
@@ -139,3 +139,112 @@ var showQuestion = function() {
 
     document.querySelector(".question-container").innerHTML = template;
 }
+
+var endGame = function() {
+    //hide questions
+    document.querySelector(".question-container").classList.add("hide");
+    //show the end container
+    document.querySelector(".end-container").classList.remove("hide");
+    //show results = score
+    document.querySelector(".score").textContent = score;
+    //stop time
+    clearInterval(timer);
+}
+
+
+
+document.querySelector(".question-container").addEventListener("click", function(event) {
+    if (event.target.classList.contains("choice")) {
+        var currentQuestion = questions[questionindex];
+        //check score
+        if (currentQuestion.answer === event.target.textContent) {
+            score++;
+        } else {
+            time -= 5;
+        }
+
+        //increase the q index
+        questionindex++;
+        //show next questions
+        if (questionindex == questions.length) {
+            endGame();
+        } else {
+            showQuestion();
+        }
+    }
+})
+
+document.querySelector("#start").addEventListener('click', function(event) {
+    //hide start elements
+    document.querySelector(".start-container").classList.add("hide");
+    //show questions elements
+    document.querySelector(".question-container").classList.remove("hide");
+    //start timer
+    timer = setInterval(function() {
+        time--
+        document.querySelector("#countdown").textContent = time;
+        if (time <= 0) {
+            endGame();
+        }
+    }, 1000);
+    //show question
+    showQuestion();
+});
+
+
+document.querySelector("#form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    //get the user input
+    var initials = document.querySelector("#initials").value;
+
+    //create newdata entry
+    var entry = {
+        init: initials,
+        score: score
+    }
+
+    //get old data
+    var data = JSON.parse(localStorage.getItem("data")) || [];
+
+    //new data in tot he old data
+    data.push(entry);
+
+    //set the data
+    localStorage.setItem("data",JSON.stringify(data));
+});var timer;
+var time = 75;
+var questionindex = 0;
+var score = 0;
+
+var showQuestion = function() {
+    var currentQuestion = questions[questionindex];
+
+    var template = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title">${currentQuestion.title}</h5>
+            <p class="card-text choice">${currentQuestion.choices[0]}</p>
+            <p class="card-text choice">${currentQuestion.choices[1]}</p>
+            <p class="card-text choice">${currentQuestion.choices[2]}</p>
+            <p class="card-text choice">${currentQuestion.choices[3]}</p>
+            </div>
+        </div>
+    `;
+
+    document.querySelector(".question-container").innerHTML = template;
+}
+
+var endGame = function() {
+    //hide questions
+    document.querySelector(".question-container").classList.add("hide");
+    //show the end container
+    document.querySelector(".end-container").classList.remove("hide");
+    //show results = score
+    document.querySelector(".score").textContent = score;
+    //stop time
+    clearInterval(timer);
+}
+
+
+
